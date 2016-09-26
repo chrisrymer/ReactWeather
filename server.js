dddce0f79ef77b330057ed2e44c09830d34f53e9
chrisrymer <rymercho@gmail.com>
2016-09-26 21:50:16 +0100

    Fix middleware for localhost

--- server.js
+++ server.js
@@ -7,12 +7,8 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use( function (req, res, next) {
-    if(PORT != 3000) {
-         if(req.headers['x-forwarded-proto'] === 'http') {
-            next();
-         } else {
-            res.redirect('http://' + req.hostname + req.url);
-         }
+    if(req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
     } else {
         next();
     }
